0d612bf98fb186dbecbc9129a712ee80734ab7ba
kirill <kirill.dudchenko@zenzero.ae>
2022-07-09 22:00:31 +0700

    add more configuration;

--- init.go
+++ init.go
@@ -1,13 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
+
+func getLogFile(name string) *os.File {
+	path := filepath.Join(os.Getenv("LOG_DIR"), name)
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		panic(err.Error())
+	}
+	return file
+}
 
 func init() {
 	// read .env
@@ -16,14 +26,18 @@ func init() {
 		panic(".env file is not located")
 	}
 
+	// config gin
+	gin.SetMode(os.Getenv("GIN_MODE"))
+	io.MultiWriter()
+	if os.Getenv("APP_ENV") == "prod" {
+		file := getLogFile("gin.log")
+		gin.DefaultWriter = io.MultiWriter(file)
+	}
+
 	// setup logger
 	log.SetFlags(log.Ltime | log.Ldate | log.Lshortfile)
 	if os.Getenv("APP_ENV") == "prod" {
-		path := filepath.Join(os.Getenv("LOG_DIR"), "general.log")
-		file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-		if err != nil {
-			panic(err.Error())
-		}
+		file := getLogFile("general.log")
 		log.SetOutput(file)
 	}
 }
